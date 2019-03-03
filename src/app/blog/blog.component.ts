cf27e16ce5eecf94128417e894233987deee0d54
KelseyMoore <moorek30@xavier.edu>
2019-03-03 16:43:37 -0500

    .

---
+++ src/app/blog/blog.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
+
+@Component({
+  selector: 'app-blog',
+  templateUrl: './blog.component.html',
+  styleUrls: [ './blog.component.css' ]
+})
+export class BlogComponent{};
